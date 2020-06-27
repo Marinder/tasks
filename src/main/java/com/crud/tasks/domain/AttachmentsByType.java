5c103b60bed738e9dd31f107f6ec0b3ad251b5c7
Marinder <dkucharski1996@gmail.com>
2020-06-27 21:44:15 +0200

    22.3

--- src/main/java/com/crud/tasks/domain/AttachmentsByType.java
+++ src/main/java/com/crud/tasks/domain/AttachmentsByType.java
@@ -1,28 +1,17 @@
 package com.crud.tasks.domain;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
-import java.util.List;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+public class AttachmentsByType {
 
-public class TrelloBoardDto {
-
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("trello")
+    private Trello trello;
 }
--- src/main/java/com/crud/tasks/domain/Badges.java
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -1,28 +1,21 @@
 package com.crud.tasks.domain;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
-import java.util.List;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+public class Badges {
 
-public class TrelloBoardDto {
+    @JsonProperty("votes")
+    private int votes;
 
-    @JsonProperty("id")
-    private String id;
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachmentsByType;
 
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
 }
