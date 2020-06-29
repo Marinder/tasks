84d37513c8431e5875be6919f4624969c1c715cb
Marinder <dkucharski1996@gmail.com>
2020-06-29 15:34:26 +0200

    heroku 23

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/trello")
+@RequestMapping("/v1/trello/")
 @CrossOrigin("*")
 public class TrelloController {
-
     @Autowired
     private TrelloClient trelloClient;
 
